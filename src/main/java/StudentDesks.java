63088042b3faa1b8a9e897c68f002137d013579b
Dominick Beck <dominick.234115@nv.ccsd.net>
2020-09-07 04:04:53 +0000

    Finished StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -14,13 +14,36 @@ class StudentDesks {
         /*
          *  your code goes here
          */
-          int desksNeeded;
-          if(numInClass1 % 2 = 1)
+          int desksNeeded = 0;
+
+          if(numInClass1 % 2 == 1)
           {
-            desksNeeded += numInClass1 / 2 + 1;
+            desksNeeded += (numInClass1 / 2) + 1;
+          }
+          else
+          {
+            desksNeeded += numInClass1 / 2;
           }
 
-          System.out.println("The amount of desks needed is" + desksNeeded);
+          if(numInClass2 % 2 == 1)
+          {
+            desksNeeded += (numInClass2 / 2) + 1;
+          }
+          else
+          {
+            desksNeeded += numInClass2 / 2;
+          }
+
+          if (numInClass3 % 2 == 1)
+          {
+            desksNeeded += (numInClass3 % 2) + 1;
+          }
+          else
+          {
+            desksNeeded += numInClass3 / 2;
+          }
+
+          System.out.println("The amount of desks needed is " + desksNeeded);
         // closing the scanner object
         scanner.close();
     }
