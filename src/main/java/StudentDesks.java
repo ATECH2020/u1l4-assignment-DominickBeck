5d14b3d7a17a27b546191bfc2baab409dc109bc2
Dominick Beck <dominick.234115@nv.ccsd.net>
2020-09-07 03:48:41 +0000

    Making progress on Studentdesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,13 +7,20 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int numInClass1 = scanner.nextInt();
+        int numInClass2 = scanner.nextInt();
+        int numInClass3 = scanner.nextInt();
 
         /*
          *  your code goes here
          */
+          int desksNeeded;
+          if(numInClass1 % 2 = 1)
+          {
+            desksNeeded += numInClass1 / 2 + 1;
+          }
 
-
+          System.out.println("The amount of desks needed is" + desksNeeded);
         // closing the scanner object
         scanner.close();
     }
--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -13,22 +13,22 @@ class Timestamps {
         int hours2 = scanner.nextInt();
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
-        int secInHour1;
-        int secInHour2;
 
         /*
          *  your code goes here
          */
-          int secInHour1 = hours * 3600;
-          int secInHour2 = hours * 3600;
+          int secInHour1 = hours1 * 3600;
+          int secInHour2 = hours2 * 3600;
           int secInMin1 = minutes1 * 60;
           int secInMin2 = minutes2 * 60;
 
           int deltaHour = secInHour2 - secInHour1;
           int deltaMin = secInMin2 - secInMin1;
-          int deltaSec = secounds2 - secounds1;
+          int deltaSec = seconds2 - seconds1;
           
-          int totalSec = delatHour + deltaMin + deltaSec;
+          int totalSec = deltaHour + deltaMin + deltaSec;
+
+          System.out.println(totalSec);
         // closing the scanner object
         scanner.close();
     }
