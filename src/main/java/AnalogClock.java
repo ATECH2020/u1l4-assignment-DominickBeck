9c201f6600b9c064f67bd2463db5d88c92a8ec3e
Dominick Beck <dominick.234115@nv.ccsd.net>
2020-09-07 05:25:47 +0000

    I finished the TimeStamps assignment

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,12 +7,21 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-
+        int currentHourDegrees = scanner.nextInt();
+        int hourHandDegrees = 2;
+        int minuteHandDegrees = 6;
+        int degreesPerHour;
+        int minutesPast;
+        int currentMinuteDegrees;
 
         /*
          *  your code goes here
          */
+          degreesPerHour = currentHourDegrees % 30;
+          minutesPast = degreesPerHour * hourHandDegrees;
+          currentMinuteDegrees = minutesPast * minuteHandDegrees;
 
+          System.out.println(currentMinuteDegrees);
         // closing the scanner object
         scanner.close();
     }
