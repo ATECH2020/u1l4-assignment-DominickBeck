1beb08be8ace9c79407fda2a03c07d77de49b56a
Dominick Beck <dominick.234115@nv.ccsd.net>
2020-09-09 16:51:05 +0000

    trying to solve what went wrong with seconds past midnight

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -7,14 +7,14 @@ class SecondsPastMidnight {
         Scanner scanner = new Scanner(System.in);
 
         //Reads int from user
-        int number = 7500;
+        int number = scanner.nextInt();
         int hours;
         int min;
 
         //Your code goes here
         hours = number / 3600;
         min = number / 60;
-        System.out.print(hours + "  " + min);
+        System.out.print(hours + " " + min);
 
         
         // closing the scanner object
